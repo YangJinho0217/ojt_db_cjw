071ef77c20c173c9285b3211b2aa650efeebe937
양진호 <>
2024-08-23 17:27:01 +0900

    프로젝트 생성단 api 제작중 프로젝트 리스트 뽑을 때 보안담당자, 개발담당자 추가해서 쿼리결과로 보여주기

--- src/loaders/multer.js
+++ src/loaders/multer.js
@@ -1,37 +1,16 @@
 const multer = require("multer");
-const fs = require('fs');
 
 // 파일 업로드 위치 지정
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const prj_id = req.body.prj_id; // prj_id를 요청 바디에서 가져오기
-    const version_id = req.body.version_id; // version_id를 요청 바디에서 가져오기
-
     // // 최종 저장 경로 지정
     cb(null, process.env.PWD + '/src/file/');
-
-    // 기본 업로드 경로
-    // const uploadPath = path.join('file', prj_id, version_id);
-
-    // // prj_id 경로가 없으면 생성
-    // if (!fs.existsSync(path.join('file', prj_id))) {
-    //   fs.mkdirSync(path.join('file', prj_id), { recursive: true });
-    // }
-
-    // // version_id 경로가 없으면 생성
-    // if (!fs.existsSync(uploadPath)) {
-    //   fs.mkdirSync(uploadPath, { recursive: true });
-    // }
-
-    // // 최종 저장 경로 지정
-    // cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     // 일단 임시로 중복저장 허용하기 위해 Date.now 사용
-    cb(null, Date.now() + '.' + file.originalname);
+    cb(null, file.originalname);
   },
 });
-
 
 /* 이미지 링크 DB 저장 시 DATE 값을 앞에 추가 후 저장 하고 있음 */
 /* DATE값 추출 function */
@@ -54,31 +33,6 @@ function getDate() {
   var fullString = dateString +  '.' + timeString
 
   return  fullString
-}
-
-function fileUpload(file, prj_id, ) {
-  const uploadPath = path.join('public', 'user');
-
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-
-  // 파일을 저장할 경로에 연도, 월, 일을 추가
-  const datePath = path.join(uploadPath, year.toString(), month, day, user_id);
-
-  // 디렉터리가 존재하지 않으면 생성
-  if (!fs.existsSync(datePath)) {
-    fs.mkdirSync(datePath, { recursive: true });
-  }
-
-  // 파일 경로 설정
-  const filePath = path.join(datePath, file.originalname);
-
-  // 파일 저장
-  fs.writeFileSync(filePath, file.buffer);
-
-  return filePath
 }
 
 const upload = multer({ storage: storage });
--- src/loaders/mysql.js
+++ src/loaders/mysql.js
@@ -10,57 +10,81 @@ const query = {
     console.log('----------------------------------------')
     console.log(sql)
     console.log('----------------------------------------')
-    const [rows, fields] = await con.query(sql).catch(async (err) => {
+    try {
+        const [rows, fields] = await con.query(sql).catch(async (err) => {
+            con.connection.release()
+            throw err
+        })
+        await con.commit()
+        return rows
+    } catch (error) {
+        await con.rollback()
+        return error
+    } finally {
         con.connection.release()
-        throw err
-    })
-
-    con.connection.release()
-    return rows
+    }
+    
   },
   select : async (nameSpace, selectId, param) => {
-      const con = await pool.getConnection(async conn => conn)
-      const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
-      console.log('----------------------------------------')
-      console.log(sql)
-      console.log('----------------------------------------')
-      const [rows, fields] = await con.query(sql).catch(async (err) => {
-          con.connection.release()
-          throw err
-      })
-
-      con.connection.release()
-      return typeof rows[0] == 'undefined' ? {} : rows[0]
+    const con = await pool.getConnection(async conn => conn)
+    const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
+    console.log('----------------------------------------')
+    console.log(sql)
+    console.log('----------------------------------------')
+    try {
+        const [rows, fields] = await con.query(sql).catch(async (err) => {
+            con.connection.release()
+            throw err
+        })
+        await con.commit()
+        return typeof rows[0] == 'undefined' ? {} : rows[0]
+    } catch (error) {
+        await con.rollback()
+        return error
+    } finally {
+        con.connection.release()
+    }
   },
   proc : async (nameSpace, selectId, param) => {
-      const con = await pool.getConnection(async conn => conn)
-      const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
-      console.log('----------------------------------------')
-      console.log(sql)
-      console.log('----------------------------------------')
-      var result = await con.query(sql).catch(async (err) => {
-          con.connection.release()
-          throw err
-      })
-
-      con.connection.release()
-      return result[0].affectedRows
+    const con = await pool.getConnection(async conn => conn)
+    const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
+    console.log('----------------------------------------')
+    console.log(sql)
+    console.log('----------------------------------------')
+    try {
+        var result = await con.query(sql).catch(async (err) => {
+            con.connection.release()
+            throw err
+        })  
+        await con.commit()
+        return result[0].affectedRows
+    } catch (error) {
+        await con.rollback()
+        return error
+    } finally {
+        con.connection.release()
+    }
   },
   value : async (nameSpace, selectId, param) => {
-      const con = await pool.getConnection(async conn => conn)
-      const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
-      console.log('----------------------------------------')
-      console.log(sql)
-      console.log('----------------------------------------')
-      const [rows, fields] = await con.query(sql).catch(async (err) => {
-          con.connection.release()
-          throw err
-      })
-
-      con.connection.release()
-      return rows[0][fields[0].name]
+    const con = await pool.getConnection(async conn => conn)
+    const sql = mapper.getStatement(nameSpace, selectId, param, {language: 'sql', indent: '  '})
+    console.log('----------------------------------------')
+    console.log(sql)
+    console.log('----------------------------------------')
+    try {
+        const [rows, fields] = await con.query(sql).catch(async (err) => {
+            con.connection.release()
+            throw err
+        }) 
+        await con.commit()
+        return rows[0][fields[0].name]
+    } catch (error) {
+        await con.rollback()
+        return error
+    } finally {
+        con.connection.release()
+    }
   },
-
 };
 
 module.exports = query;
